c51faf2e785e2457cdf9ea377f7377ea81a83dc0
karlapraveenkumar <131789praveen@gmail.com>
2024-05-01 21:08:36 +0530

    Food app final code

---
+++ food_app/src/components/MenuShimmer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const MenuShimmer = () => {
+  return (
+    <div>
+        <div className="bg-green-50 w-6/12 h-10 mx-auto my-4 p-4 shadow-lg ">
+            <div 
+                className="flex justify-between cursor-pointer"
+            >
+            </div>
+        </div>
+        <div className="bg-green-50 w-6/12 h-10 mx-auto my-4 p-4 shadow-lg ">
+            <div 
+                className="flex justify-between cursor-pointer"
+            >
+            </div>
+        </div>
+        <div className="bg-green-50 w-6/12 h-10 mx-auto my-4 p-4 shadow-lg ">
+            <div 
+                className="flex justify-between cursor-pointer"
+            >
+            </div>
+        </div>
+        <div className="bg-green-50 w-6/12 h-10 mx-auto my-4 p-4 shadow-lg ">
+            <div 
+                className="flex justify-between cursor-pointer"
+            >
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default MenuShimmer
---
+++ food_app/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const CDN_URL = 
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL = 
+    "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
+
+export const CART_EMPTY_IMAGE = "https://i.pinimg.com/originals/2e/ac/fa/2eacfa305d7715bdcd86bb4956209038.png";
+
+//export default {CDN_URL,LOGO_URL};
