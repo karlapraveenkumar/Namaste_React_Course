ccca5c997f7b9fb570f6f15953a261a54047d6e8
karlapraveenkumar <131789praveen@gmail.com>
2025-10-19 15:27:09 +0530

    Moved 3 projects

--- Netflix GPT/netflix/src/utils/openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from 'openai';
-import { OPENAI_KEY } from './constants';
-
-const openai = new OpenAI({
-  apiKey: OPENAI_KEY, // This is the default and can be omitted and we have changed the code little bit
-  dangerouslyAllowBrowser : true,
-});
-
-export default openai;
